db2a349235cdb578d01eec18781da2905b724520
=Oier Marañón <o.maranonocejo@ikasle.eus>
2026-03-12 14:07:17 +0100

    xmlKudeatzailea aldatuta

--- PROG/src/utils/XmlKudeatzailea.java
+++ PROG/src/utils/XmlKudeatzailea.java
@@ -9,15 +9,11 @@ public class XmlKudeatzailea {
 
     public boolean esportatuXML(Federazioa federazioa, String rutaFitxategia) {
         try {
-            // Creamos el contexto. 
-            // NOTA: Si tienes herencia en Jokalari/Talde, añádelas aquí también.
             JAXBContext context = JAXBContext.newInstance(Federazioa.class);
             Marshaller marshaller = context.createMarshaller();
             
-            // Formatear el XML para que se vea bonito y tabulado
             marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
 
-            // Generar el archivo
             File file = new File(rutaFitxategia);
             marshaller.marshal(federazioa, file);
             
